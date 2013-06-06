b1634dfb113393e407001e0e86ceb0f2b88adc8c
José Ribeiro <jlbribeiro@gmail.com>
2013-06-06 19:05:47 +0100

    [src] Multiple process MPI example fixed (code clarification); fixed makefile

--- projects/project2/src/main.c
+++ projects/project2/src/main.c
@@ -3,28 +3,37 @@
 #include <string.h>
 
 #define MSG_BUFFER_SIZE 32
+#define N_PROCESSES 9
 
 int main (int argc, char **argv)
 {
 	char message[MSG_BUFFER_SIZE];
 	int my_rank;
 
+	int dest_rank, master_rank = 0;
+
+	int message_tag = 1;
+
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	if (my_rank == 0)
+	if (my_rank == master_rank)
 	{
 		/* process 0 */
-		strcpy(message, "Hello World!");
-		MPI_Send(message, strlen(message) + 1, MPI_CHAR, 1, 99, MPI_COMM_WORLD);
-		printf("[%d] Sent: %s\n", my_rank, message);
+		for (dest_rank = 1; dest_rank < N_PROCESSES; dest_rank++)
+		{
+			sprintf(message, "%d says: \"Hello, process %d!\"", master_rank, dest_rank);
+			MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest_rank, message_tag, MPI_COMM_WORLD);
+
+			printf("[%d] Sent to %d: %s\n", my_rank, dest_rank, message);
+		}
 	}
 	else
 	{
-		/* process 1 */
-		MPI_Recv(message, MSG_BUFFER_SIZE, MPI_CHAR, 0, 99, MPI_COMM_WORLD, &status);
+		/* process 1-8 */
+		MPI_Recv(message, MSG_BUFFER_SIZE, MPI_CHAR, master_rank, message_tag, MPI_COMM_WORLD, &status);
 		printf("[%d] Received: %s\n", my_rank, message);
 	}
 
