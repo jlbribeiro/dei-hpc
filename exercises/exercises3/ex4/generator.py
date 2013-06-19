afc4525c3793e458f8449694fe66c160b69dc6e9
João Ferreira <jpbat@student.dei.uc.pt>
2013-06-19 18:23:17 +0100

    changed generator acordingly to pdf

--- exercises/exercises3/ex4/generator.py
+++ exercises/exercises3/ex4/generator.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from random import randint
 
-def main():
+def generate(degree, n_tests):
 	
 	f = open('input', 'w')
 
-	degree = randint(5,10)
 	f.write(str(degree) + ' ')
 	for i in xrange(degree + 1):
 		x = 0
@@ -15,13 +15,18 @@ def main():
 		f.write(str(x) + ' ')
 	f.write('\n')
 
-	n_tests = randint(5,10)
 	f.write(str(n_tests) + '\n')
 	for i in xrange(n_tests):
 		f.write(str(randint(1,10)) + '\n')
 
 	f.close()
 
+
+def main():
+	if (len(sys.argv) < 3):
+		print 'Wrong usage!'
+		return
+	generate(int(sys.argv[1]), int(sys.argv[2]))
 
 if __name__ == '__main__':
 	main()
